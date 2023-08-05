ac4f41aa5eb357eea02c72594b4c407104730f43
Kurbaan Jhan <126372736+KurbaanJhan@users.noreply.github.com>
2023-08-05 16:30:42 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,6 @@
 import pygame
-import sys
-import main_menu
 
-pygame.init()
-
-pygame.init()
-WIDTH, HEIGHT = 1600, 800
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Fire it Out")
-white = (255, 255, 255)
-black = (255, 0, 255)
-scaling_factor = 2
+#Load assets only once and use it in your game loop
 sky = pygame.image.load("Sky.png")
 ground = pygame.image.load("ground.png")
 platform_1 = pygame.image.load("ground1.png")
@@ -19,77 +9,105 @@ platform_4 = pygame.image.load("ground4.png")
 tree_bush = pygame.image.load("tree_bush.png")
 tree_double = pygame.image.load("tree_double.png")
 tree_pine_cone = pygame.image.load("tree_pine_cone.png")
-clok = pygame.time.Clock()
+standing = pygame.transform.scale(pygame.image.load("assets/stickman1.png"), (32, 32))
+jump = pygame.transform.scale(pygame.image.load("assets/stickmanrunning2.png"), (32, 32))
+
+#Initialize player values
 player = pygame.Rect((300, 250, 50, 50))
 xpos, ypos = 400, 600
 jumping = False
 GRAV = 1
 JHEIGHT = 20
 YVELO = JHEIGHT
-standing = pygame.transform.scale(pygame.image.load("assets/stickman1.png"), (32, 32))
-jump = pygame.transform.scale(
-    pygame.image.load("assets/stickmanrunning2.png"), (32, 32)
-)
-stikkrect = standing.get_rect(center=(xpos, ypos))
 
-WIN.blit(sky, (0, 0))
-WIN.blit(ground, (0, 610))
+def main_game(screen):
+    global xpos, ypos, YVELO, jumping
 
+    clock = pygame.time.Clock()  # create a clock object for timing
 
-WIN.blit(platform_1, (1100, 360))
-WIN.blit(tree_double, (1100, 255))
-WIN.blit(tree_pine_cone, (1200, 213))
-WIN.blit(tree_bush, (1350, 205))
-WIN.blit(tree_pine_cone, (1450, 213))
-WIN.blit(tree_double, (1275, 255))
+    run = True
+    while run:
+        # handle every event since the last frame.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-WIN.blit(platform_3, (50, 360))
-WIN.blit(tree_bush, (50, 205))
-WIN.blit(tree_bush, (150, 205))
-WIN.blit(tree_double, (250, 255))
-WIN.blit(tree_pine_cone, (350, 213))
+        # clear the screen
+        screen.fill((0, 0, 0))
 
+        screen.blit(sky, (0, 0))
+        screen.blit(ground, (0, 610))
+        screen.blit(platform_1, (1100, 360))
+        screen.blit(tree_double, (1100, 255))
+        screen.blit(tree_pine_cone, (1200, 213))
+        screen.blit(tree_bush, (1350, 205))
+        screen.blit(tree_pine_cone, (1450, 213))
+        screen.blit(tree_double, (1275, 255))
 
-WIN.blit(platform_4, (530, 170))
-WIN.blit(tree_bush, (540, 17))
-WIN.blit(tree_double, (640, 65))
-WIN.blit(tree_pine_cone, (740, 25))
-WIN.blit(tree_pine_cone, (840, 25))
-WIN.blit(tree_bush, (940, 17))
+        screen.blit(platform_3, (50, 360))
+        screen.blit(tree_bush, (50, 205))
+        screen.blit(tree_bush, (150, 205))
+        screen.blit(tree_double, (250, 255))
+        screen.blit(tree_pine_cone, (350, 213))
 
+        screen.blit(platform_4, (530, 170))
+        screen.blit(tree_bush, (540, 17))
+        screen.blit(tree_double, (640, 65))
+        screen.blit(tree_pine_cone, (740, 25))
+        screen.blit(tree_pine_cone, (840, 25))
+        screen.blit(tree_bush, (940, 17))
 
-WIN.blit(tree_double, (410, 505))
-WIN.blit(tree_pine_cone, (500, 465))
-WIN.blit(tree_bush, (600, 457))
-WIN.blit(tree_pine_cone, (700, 465))
+        screen.blit(tree_double, (410, 505))
+        screen.blit(tree_pine_cone, (500, 465))
+        screen.blit(tree_bush, (600, 457))
+        screen.blit(tree_pine_cone, (700, 465))
 
+        screen.blit(tree_bush, (900, 457))
+        screen.blit(tree_bush, (1000, 457))
+        screen.blit(tree_double, (1100, 505))
+        screen.blit(tree_double, (100, 505))
+        screen.blit(tree_double, (1400, 505))
+        screen.blit(tree_double, (200, 505))
 
-WIN.blit(tree_bush, (900, 457))
-WIN.blit(tree_bush, (1000, 457))
-WIN.blit(tree_double, (1100, 505))
-WIN.blit(tree_double, (100, 505))
-WIN.blit(tree_double, (1400, 505))
-WIN.blit(tree_double, (200, 505))
-
-pygame.display.update()
-while True:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    keyspress = pygame.key.get_pressed()
-    if keyspress[pygame.K_SPACE]:
-        jumping = True
-    if jumping:
-        ypos -= YVELO
-        YVELO -= GRAV
+        keyspress = pygame.key.get_pressed()
+        if keyspress[pygame.K_SPACE]:
+            jumping = True
+        if jumping:
+            ypos -= YVELO
+            YVELO -= GRAV
         if YVELO < -JHEIGHT:
             jumping = False
             YVELO = JHEIGHT
-        stikkrect = jump.get_rect(center=(xpos, ypos))
-        WIN.blit(jump, stikkrect)
-    else:
-        stikkrect = standing.get_rect(center=(xpos, ypos))
-        WIN.blit(standing, stikkrect)
-    clok.tick(60)
+            stikkrect = jump.get_rect(center=(xpos, ypos))
+            screen.blit(jump, stikkrect)
+        else:
+            stikkrect = standing.get_rect(center=(xpos, ypos))
+            screen.blit(standing, stikkrect)
+
+
+        # update the display to show the new frame
+        pygame.display.flip()
+
+        # cap the framerate at 60 FPS.
+        clock.tick(60)
+
+def options(screen):
+    pygame.init
+    size = (1600, 800)
+    screen = pygame.display.set_mode(size)
+    done = False
+
+
+
+    while not done:
+        # --- Main event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  # When user clicked close
+                done = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  # When user pressed ESC key
+                    done = True
+
+
+
+    pygame.quit()
